aba47d5075a583ab948fc1a221227c83512ff892
Francis Wang <frwang@mit.edu>
2020-07-30 09:25:21 +0800

    Adopt apache license

--- common/src/main/kotlin/com/verik/uvm/tlm1/uvm_sqr_connections.kt
+++ common/src/main/kotlin/com/verik/uvm/tlm1/uvm_sqr_connections.kt
@@ -1,13 +1,26 @@
+/*
+ * Copyright 2020 Francis Wang
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 @file:Suppress("UNUSED_PARAMETER", "unused")
 
 package com.verik.uvm.tlm1
 
-import com.verik.common.extern
-import com.verik.common.task
+import com.verik.common.*
 import com.verik.uvm.base._uvm_port_base
 import com.verik.uvm.seq._uvm_sequence_item
-
-// Copyright (c) 2020 Francis Wang
 
 @extern class _uvm_seq_item_pull_port<_REQ: _uvm_sequence_item>(REQ: _REQ): _uvm_port_base<_REQ, _REQ>(REQ, REQ) {
 
--- core/src/main/kotlin/com/verik/core/kt/KtRuleParser.kt
+++ core/src/main/kotlin/com/verik/core/kt/KtRuleParser.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020 Francis Wang
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.verik.core.kt
 
 import com.verik.antlr.KotlinLexer
@@ -8,8 +24,6 @@ import org.antlr.v4.runtime.*
 import org.antlr.v4.runtime.tree.ParseTree
 import org.antlr.v4.runtime.tree.TerminalNode
 import java.util.*
-
-// Copyright (c) 2020 Francis Wang
 
 class KtRuleParser {
 
